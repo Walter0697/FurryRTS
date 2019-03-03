1084807f137e6cf163f83f29972ec70f3c305250
Walter Cheng <walterpakwai@live.com>
2019-03-02 19:32:46 -0500

    set up units movement

---
+++ Assets/Script/Units/Rabbit.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabbit : Mobile
+{
+    private Rigidbody rb;
+    private Animator anim;
+
+    public float speed = 1;
+
+    public Vector3 movement;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(0, 0, speed);
+        rb.position += movement * Time.deltaTime;
+        checkAnimate();
+    }
+
+    private void checkAnimate()
+    {
+        if (movement.magnitude == 0)
+        {
+            if (anim.GetInteger("AnimIndex") != 0) anim.SetTrigger("Next");
+            anim.SetInteger("AnimIndex", 0);
+        }
+        else
+        {
+            if (anim.GetInteger("AnimIndex") != 1) anim.SetTrigger("Next");
+            anim.SetInteger("AnimIndex", 1);
+        }
+    }
+}
---
+++ Assets/Script/template/Generatable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generatable : Immobile
+{
+    public Mobile generateUnit;
+    public float spawnTime = 5f;
+    private float cooldown = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown += Time.deltaTime;
+        if (cooldown >= spawnTime)
+        {
+            cooldown -= spawnTime;
+            Debug.Log("spawn!");
+            Mobile mob = Instantiate(generateUnit) as Mobile;
+            mob.transform.position = transform.position;
+        }
+    }
+}
