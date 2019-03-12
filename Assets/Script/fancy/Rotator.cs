feb6d0f004838f7a73ed2a81e8a96e38442d42b4
Walter0697 <walterpakwai@live.com>
2019-03-12 01:07:59 -0400

    adding base

---
+++ Assets/Script/fancy/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = transform.rotation * Quaternion.Euler(speed.x * Time.deltaTime, speed.y * Time.deltaTime, speed.z * Time.deltaTime);
+    }
+}
